fe8c2e38c5bfad694f18d29553884f0c353f61af
Marta M <marmacan.upv@gmail.com>
2023-01-18 14:10:26 +0100

    linkeamos formulario y bbdd

--- routes/news.js
+++ routes/news.js
@@ -1,16 +1,31 @@
 const express = require('express');
+const { request } = require('../app');
 const router = express.Router();
 const dbconnection = require('../config/dbconnection')
+const connection = dbconnection()
+
 
 /* GET news page. */
 router.get('/', function(req, res, next) {
     // res.send('News!!')
-    const connection = dbconnection()
     connection.query("SELECT * FROM news", (error, result) => {
         res.render('news/news.ejs', {
             news: result
         });
     })
+});
+
+router.post('/', function(req, res, next) {
+    
+    // console.log(req.body)  // --> BUENO HACERLO PARA SABER QUE FUNCIONA ANTES DE SEGUIR
+    const {title, news} = req.body
+    connection.query("INSERT INTO news SET ?", {
+        title,
+        news
+    }, (error, result) => {
+        res.redirect('/news')
+    })
+
 });
 
 module.exports = router;
