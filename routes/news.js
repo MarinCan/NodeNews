3822512d6efe4f201875430b13c3ad15eb3736be
Marta M <marmacan.upv@gmail.com>
2023-01-18 13:49:26 +0100

    Se printean los datos de la BBDD desde news.js

--- routes/news.js
+++ routes/news.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const dbconnection = require('../config/dbconnection')
 
-/* GET home page. */
+/* GET news page. */
 router.get('/', function(req, res, next) {
     // res.send('News!!')
-    res.render('news/news.ejs');
+    const connection = dbconnection()
+    connection.query("SELECT * FROM news", (error, result) => {
+        res.render('news/news.ejs', {
+            news: result
+        });
+    })
 });
 
 module.exports = router;
